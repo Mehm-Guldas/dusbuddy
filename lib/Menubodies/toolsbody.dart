532899684c3ddf62b7422e8ba01af566cbc14d90
Mehm-Guldas <62309819+Mehm-Guldas@users.noreply.github.com>
2023-04-27 02:21:53 +0300

    B

--- lib/Menubodies/toolsbody.dart
+++ lib/Menubodies/toolsbody.dart
@@ -24,8 +24,8 @@ class AraclarPageBody extends StatelessWidget {
                       height: 200,
                       width: 200,
                     ),
-                    SizedBox(height: 6,),
-                    const Text('Puan Hesaplama',
+                    SizedBox(height: 6,),//puan hesaplamayı sadece osym ayarlayacakmış
+                    const Text('Net Hesaplama',//biz 4 yanlış bir doğruyu goturecek şekilde net sayısını hesaplayacagiz
                       style: TextStyle(fontSize: 24, color: Colors.black87),
                     )
                   ],
@@ -93,3 +93,23 @@ class AraclarPageBody extends StatelessWidget {
       ],);
   }
 }
+//TEMEL BİLİMLER
+//Anatomi 6 soru
+// Histoloji ve Embriyoloji 4 soru
+// Fizyoloji 6 soru
+// Tıbbi Biyokimya 6 soru
+// Tıbbi Mikrobiyoloji 6 soru
+// Tıbbi Patoloji 4 soru
+// Tıbbi Farmakoloji 4 soru
+// Tıbbi Biyoloji veGenetik 4soru
+
+//TEMEL TIP DERSLERİ
+// Protetik Diş Tedavisi 10 soru
+// Restoratif Diş Tedavisi 10 soru
+// Ağız, Diş ve Çene Cerrahisi 10 soru
+// Ağız, Diş ve Çene Radyolojisi 10 soru
+// Periodontoloji 10 soru
+// Endodonti 10 soru
+// Ortodonti 10 Soru
+// Çocuk Diş Hekimliği 10 soru
+//toplamda 120 soru aynı deneme sınavı içinde
